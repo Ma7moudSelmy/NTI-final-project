8634496f774272a4fa9992e3f4291caa3b6c529c
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-05-08 12:39:36 +0300

    Add new images and implement Splash screen with navigation to Home

---
+++ lib/Screen/Splash.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(100.0),
+          child: Image.asset("assets/images/Grabber.png"),
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Welcome to Home Screen")));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projcet_nti_final/Screen/Splash.dart';
 
 void main() {
   runApp(MyWidget());
@@ -9,6 +10,6 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home(), debugShowCheckedModeBanner: false);
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Splash());
   }
 }
