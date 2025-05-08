a3ab7d24d9a861896c0872318eb9f505dfc2900e
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-05-08 12:39:16 +0300

    Add logo and asset images; implement Home widget in Flutter app

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyWidget());
-}
-
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home(), debugShowCheckedModeBanner: false);
+    return Scaffold();
   }
 }
