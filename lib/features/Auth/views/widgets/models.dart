b4d69440a31a372ea28631a432283c8260055d3c
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-05-17 18:10:23 +0300

    Refactor code

--- lib/features/Auth/views/widgets/models.dart
+++
@@ -1,5 +0,0 @@
-class categorymodel {
-  final String name;
-  final String image;
-  categorymodel({required this.name, required this.image});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:projcet_nti_final/features/home/views/widgets/Splash.dart';
+import 'dart:ui' show PointerDeviceKind;
+import 'package:e_commerce_flutter/core/app_theme.dart';
+import 'package:e_commerce_flutter/src/view/screen/home_screen.dart';
 
-void main() {
-  runApp(MyWidget());
-}
+void main() => runApp(const MyApp());
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Splash());
+    return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+      theme: AppTheme.lightAppTheme,
+    );
   }
 }
